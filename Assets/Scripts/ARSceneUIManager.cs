b1eb0060641d98d6731badcf0322350cdabf4062
obamski <obrebskirafal@gmail.com>
2018-11-16 23:56:58 +0100

    Added the ar to vr transition scenes.

---
+++ Assets/Scripts/ARSceneUIManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ARSceneUIManager : MonoBehaviour
+{
+
+
+    public void LoadVRScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/ARVRScenesTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ARVRScenesTransition : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(ActivateVR());
+    }
+
+    IEnumerator ActivateVR()
+    {
+        XRSettings.LoadDeviceByName("Cardboard");
+        yield return null;
+        XRSettings.enabled = true;
+    }
+
+}
