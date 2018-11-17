bf9972156c66edfd356df1a109cbe78906bd3b14
obamski <obrebskirafal@gmail.com>
2018-11-17 10:29:07 +0100

    Changes made to UI on ARScene (added animations) and photo saving

--- Assets/Scripts/ARSceneUIManager.cs
+++ Assets/Scripts/ARSceneUIManager.cs
@@ -1,19 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 namespace EasyAR
 {
 
     public class ARSceneUIManager : MonoBehaviour
     {
-
+        bool buttonsVisible;
+        [SerializeField]
+        private Animator buttonsAnimator;
+        [SerializeField]
+        private Text hideShowButtonLabel;
+        private void Start()
+        {
+            buttonsVisible = true;
+        }
         public void LoadVRScene(bool isVr)
         {
             ARVRScenesTransition.isVr = isVr;
             ARObjectsSave save = FindObjectOfType<ARObjectsSave>();
             save.SaveAllARFurniture();
             SceneManager.LoadScene(1);
+        }
+
+        public void HideShowButtons()
+        {
+            if (buttonsVisible)
+            {
+                buttonsAnimator.Play("HideButtonsAR");
+                hideShowButtonLabel.text = "SHOW";
+                buttonsVisible = false;
+            }
+            else
+            {
+                buttonsAnimator.Play("ShowButtons");
+                hideShowButtonLabel.text = "HIDE";
+                buttonsVisible = true;
+
+            }
         }
     }
 
