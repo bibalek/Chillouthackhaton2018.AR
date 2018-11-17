01f846747482b9c5b72a34208ac129312d9d1c67
obamski <obrebskirafal@gmail.com>
2018-11-17 08:45:24 +0100

    Working on VR teleportation walking

--- Assets/Scripts/VRCameraGaze.cs
+++ Assets/Scripts/VRCameraGaze.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VRCameraGaze : MonoBehaviour
 {
-
+    [SerializeField]
+    private Image circleToFill;
+    [SerializeField]
+    private Image fadePanel;
     public Camera viewCamera;
     bool isGazing;
+    bool isTransition = false;
+    [SerializeField]
+    private GameObject gazePoint;
+
+    private GameObject currentTeleportPoint;
 
     private void Start()
     {
@@ -16,19 +25,64 @@ public class VRCameraGaze : MonoBehaviour
 
     void Update()
     {
-        // Create a gaze ray pointing forward from the camera
-        Ray ray = new Ray(viewCamera.transform.position, viewCamera.transform.rotation * Vector3.forward);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if (!isTransition)
         {
-            if (hit.collider.tag == "teleport")
+            // Create a gaze ray pointing forward from the camera
+            Ray ray = new Ray(gazePoint.transform.position, gazePoint.transform.rotation * Vector3.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                isGazing = true;
+                if (hit.collider.tag == "teleport")
+                {
+                    isGazing = true;
+                    currentTeleportPoint = hit.transform.gameObject;
+                }
+                else
+                {
+                    isGazing = false;
+                    circleToFill.fillAmount = 0;
+                }
+            }
+            else
+            {
+                isGazing = false;
+                circleToFill.fillAmount = 0;
+            }
+
+            if (isGazing)
+            {
+                circleToFill.fillAmount += 0.01f;
+                if (circleToFill.fillAmount >= 1)
+                {
+                    isGazing = false;
+                    circleToFill.fillAmount = 0;
+                    circleToFill.gameObject.SetActive(false);
+                    StartCoroutine(MoveToTeleportPoint());
+                }
             }
         }
-        else
+    }
+
+    IEnumerator MoveToTeleportPoint()
+    {
+        byte alpha = 0;
+        isTransition = true;
+        while (fadePanel.color.a < 1)
         {
-            isGazing = false;
+            alpha++;
+            fadePanel.color = new Color32(0, 0, 0, alpha);
+            yield return new WaitForSeconds(Time.deltaTime);
         }
+        currentTeleportPoint.GetComponent<TeleportSphereWithCamera>().camera.SetActive(true);
+        gameObject.SetActive(false);
+        // gameObject.transform.position = new Vector3(currentTeleportPoint.transform.position.x, transform.position.y, currentTeleportPoint.transform.position.z); ;
+        while (fadePanel.color.a > 0)
+        {
+            alpha--;
+            fadePanel.color = new Color32(0, 0, 0, alpha);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        circleToFill.gameObject.SetActive(true);
+        isTransition = false;
     }
 }
